601f97c90c0fd41a89ea51168a0fbff076599d13
jeffgicharu <jeff.kaharu@students.jkuat.ac.ke>
2024-06-11 18:22:17 +0300

    Function to create the Grid

--- main.js
+++ main.js
@@ -5,7 +5,29 @@ function getRandomColor(){
     return `rgb(${r},${g},${b})`;
 }
 
+function createGrid(size){
+    const container=document.querySelector(".container");
+    container.innerHTML="";
+    const squareSize=400/size;
 
+    for(let i=0;i<size*size;i++){
+        const square=document.createElement("div");
+        square.style.width=`${squareSize}px`;
+        square.style.height=`${squareSize}`;
+        
+        square.style.opacity="1";
+
+        square.addEventListener("mouseover",()=>{
+            let currentOpacity=parseFloat(square.style.opacity);
+            square.style.backgroundColor=getRandomColor();
+            if(currentOpacity>0){
+                square.style.opacity=currentOpacity-0.1;
+            }
+        });
+        container.appendChild(square);
+    }
+
+}
 
 document.getElementById("resetButton").addEventListener("click", () => {
     let size;
